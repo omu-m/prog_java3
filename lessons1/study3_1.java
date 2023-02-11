d2c3ad9a392d486dd01d08711f69d222eaef5967
MoekaOmura <110664281+omu-m@users.noreply.github.com>
2023-02-11 02:08:00 +0000

    [lessons1]メソッド（１）

---
+++ lessons1/study3_1.java
@@ -0,0 +1,10 @@
+class study3_1 {
+  public static void main(String[] args) {
+    hello();
+  }
+
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
+  }
+}
---
+++ lessons1/study3_2.java
@@ -0,0 +1,11 @@
+class study3_2 {
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出してください
+    printData();
+  }
+
+  // printDataメソッドを定義してください
+  public static void printData() {
+    System.out.println("私の名前はKate Jonesです");
+  }
+}
